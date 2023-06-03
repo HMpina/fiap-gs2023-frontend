bd28109e4e7f52a931d317045bf6fb9a165194ae
HMpina <henrique.marques.pina@gmail.com>
2023-06-03 18:32:07 -0300

    removendo css náo utilizado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, Card, Button, Carousel } from 'react-bootstrap';
 import { useHistory, useParams } from "react-router-dom"
-import "./index.css";
 import api from "../../services/api";
 
 interface IHint {
