2d9eac8886a5906924d0ddbe9f0e6c07f7e37d9f
HMpina <henrique.marques.pina@gmail.com>
2023-06-03 00:20:41 -0300

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
-import axios from 'axios'
+import axios from "axios"
+ 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://fiap-globalsolut2023-backend.herokuapp.com/'
 })
+ 
 export default api;
