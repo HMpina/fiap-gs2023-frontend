6c0b1aa735627203d8b122146b735487afdecb3b
HMpina <henrique.marques.pina@gmail.com>
2023-06-04 16:57:56 -0300

    complementando footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "../Header/index.css"
+import { BsGithub } from "react-icons/bs"; // Importação de ícones
+
 
 const Footer: React.FC = () => {
   return (
     <footer className="text-center mt-5 py-4 bg-green1">
       <Container>
         <p>&copy; 2023 FIAPlantação. Todos os direitos reservados.</p>
+        <p className="mt-2">Henrique Aslan Marques Pina</p>
+        <p>RA92542</p>
+        <div>
+        <BsGithub /> HMpina
+        </div>
       </Container>
     </footer>
   );
