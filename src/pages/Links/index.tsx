dad06432bc563d4cc77c0c9b11345982ba0e1712
HMpina <henrique.marques.pina@gmail.com>
2023-06-04 17:23:10 -0300

    layut da página de links

--- src/pages/Links/index.tsx
+++ src/pages/Links/index.tsx
@@ -7,25 +7,33 @@ import { SiHeroku } from 'react-icons/si';
 const Links: React.FC = () => {
     return (
         <Container>
-          <h2>Links importantes</h2>
+          <h2 className="mt-4">Links importantes</h2>
+          <p className="mt-4">
+            Confira nossos repositório no GitHub:
+          </p>
           <p>
-        Confira nossos repositório no GitHub:
-        <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
-          <BsGithub /> frontend
-        </a>
-        <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
-          <BsGithub /> backend
-        </a>
-      </p>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <BsGithub /> frontend
+            </a>
+          </p>
+          <p>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <BsGithub /> backend
+            </a>
+          </p>
           <p className="my-4">
-        Veja também em produção:
-        <a href="https://fiap-gs-frontend.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-          <SiHeroku /> frontend
-        </a>
-        <a href="https://fiap-globalsolut2023-backend.herokuapp.com/hints" target="_blank" rel="noopener noreferrer">
-          <SiHeroku /> backend
-        </a>
-      </p>
+            Veja também em produção:
+          </p>
+          <p>
+            <a href="https://fiap-gs-frontend.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              <SiHeroku /> frontend
+            </a>
+          </p>
+          <p>
+            <a href="https://fiap-globalsolut2023-backend.herokuapp.com/hints" target="_blank" rel="noopener noreferrer">
+              <SiHeroku /> backend
+            </a>
+          </p>
 
         </Container>
       );
