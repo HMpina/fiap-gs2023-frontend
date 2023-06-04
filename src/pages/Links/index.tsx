1a82b8355910553c0b6b21295fdc057eeddda20f
HMpina <henrique.marques.pina@gmail.com>
2023-06-04 15:32:46 -0300

    ajustando cores

--- src/pages/Links/index.tsx
+++ src/pages/Links/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Container } from 'react-bootstrap';
  
 const Links: React.FC = () => {
-    return <h1>Página Links</h1>
-}
- 
+    return (
+        <Container>
+          <h2>Links importantes</h2>
+          <p>
+            Somos a FIAPlantação, uma equipe apaixonada pela agricultura sustentável e comprometida em promover um futuro mais verde e saudável. Nossa jornada começou com a visão de criar um ambiente agrícola que respeite a natureza, conserve os recursos naturais e forneça alimentos saudáveis para todos.
+          </p>
+          <p>
+            Aqui, acreditamos que a agricultura sustentável é a chave para enfrentar os desafios ambientais e alimentares que o mundo enfrenta atualmente. Trabalhamos em estreita colaboração com agricultores, pesquisadores e comunidades para desenvolver e compartilhar práticas inovadoras que minimizem o impacto ambiental, preservem a biodiversidade e promovam a saúde do solo.
+          </p>
+          <p>
+            Nossa equipe é formada por especialistas em diferentes áreas, como permacultura, agroecologia e agricultura orgânica. Combinamos o conhecimento tradicional com as mais recentes descobertas científicas para criar soluções práticas e sustentáveis para os desafios agrícolas.
+          </p>
+          <p>
+            Além disso, valorizamos a educação e a conscientização. Realizamos workshops, palestras e programas de capacitação para disseminar informações sobre práticas agrícolas sustentáveis e incentivar a participação da comunidade.
+          </p>
+          <p>
+            Estamos comprometidos em construir um futuro onde a agricultura e o meio ambiente possam prosperar juntos. Junte-se a nós nessa jornada rumo a um mundo mais sustentável e descubra como você pode fazer a diferença através de práticas agrícolas responsáveis. Juntos, podemos cultivar um futuro melhor para as gerações presentes e futuras.
+          </p>
+        </Container>
+      );
+    }
+    
 export default Links;
--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const Sobre: React.FC = () => {
   return (
     <Container>
-      <h2>Sobre a empresa</h2>
+      <h2>Sobre nós</h2>
       <p>
         Somos a FIAPlantação, uma equipe apaixonada pela agricultura sustentável e comprometida em promover um futuro mais verde e saudável. Nossa jornada começou com a visão de criar um ambiente agrícola que respeite a natureza, conserve os recursos naturais e forneça alimentos saudáveis para todos.
       </p>
