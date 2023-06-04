4eb7dbe1cfc96fee896dc02225154410041d576d
HMpina <henrique.marques.pina@gmail.com>
2023-06-04 17:06:49 -0300

    inserindo conteudo na página Links

--- src/pages/Links/index.tsx
+++ src/pages/Links/index.tsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { Container } from 'react-bootstrap';
- 
+import { BsGithub } from "react-icons/bs";
+import { IoLogoHeroku } from 'react-icons/io';
+
+
 const Links: React.FC = () => {
     return (
         <Container>
           <h2>Links importantes</h2>
           <p>
-            Somos a FIAPlantação, uma equipe apaixonada pela agricultura sustentável e comprometida em promover um futuro mais verde e saudável. Nossa jornada começou com a visão de criar um ambiente agrícola que respeite a natureza, conserve os recursos naturais e forneça alimentos saudáveis para todos.
-          </p>
-          <p>
-            Aqui, acreditamos que a agricultura sustentável é a chave para enfrentar os desafios ambientais e alimentares que o mundo enfrenta atualmente. Trabalhamos em estreita colaboração com agricultores, pesquisadores e comunidades para desenvolver e compartilhar práticas inovadoras que minimizem o impacto ambiental, preservem a biodiversidade e promovam a saúde do solo.
-          </p>
-          <p>
-            Nossa equipe é formada por especialistas em diferentes áreas, como permacultura, agroecologia e agricultura orgânica. Combinamos o conhecimento tradicional com as mais recentes descobertas científicas para criar soluções práticas e sustentáveis para os desafios agrícolas.
-          </p>
-          <p>
-            Além disso, valorizamos a educação e a conscientização. Realizamos workshops, palestras e programas de capacitação para disseminar informações sobre práticas agrícolas sustentáveis e incentivar a participação da comunidade.
-          </p>
-          <p>
-            Estamos comprometidos em construir um futuro onde a agricultura e o meio ambiente possam prosperar juntos. Junte-se a nós nessa jornada rumo a um mundo mais sustentável e descubra como você pode fazer a diferença através de práticas agrícolas responsáveis. Juntos, podemos cultivar um futuro melhor para as gerações presentes e futuras.
-          </p>
+        Confira nossos repositório no GitHub:
+        <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+          <BsGithub /> frontend
+        </a>
+        <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+          <BsGithub /> backend
+        </a>
+      </p>
+          <p className="my-4">
+        Veja também em produção:
+        <a href="https://fiap-gs-frontend.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+          <IoLogoHeroku /> frontend
+        </a>
+        <a href="https://fiap-globalsolut2023-backend.herokuapp.com/hints" target="_blank" rel="noopener noreferrer">
+          <IoLogoHeroku /> backend
+        </a>
+      </p>
+
         </Container>
       );
     }
